5e18a8990965c6b4067ee95f56cbaa708b7f371a
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2022-05-05 21:18:04 -0300

    docs: Set timezone project to UTC-03

--- api/api/settings.py
+++ api/api/settings.py
@@ -104,12 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/

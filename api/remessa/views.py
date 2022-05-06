ecd1ca77e3ca499fdafe6cbcc2dfdd42d3aa09ed
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2022-05-05 21:27:05 -0300

    feat: Save received data on Transacao table

--- api/remessa/views.py
+++ api/remessa/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from remessa.models import Transacao, TipoTransacao
 from .utils import CNAB
 
 
@@ -9,5 +11,22 @@ def importar_remessa(request):
         upload = request.FILES['file']
 
         remessa = CNAB(arquivo=upload)
+        if remessa:
+            for transacao in remessa.transacoes:
+                data = transacao['dt_cadastro']
+                hora = transacao.pop('hora')
+
+                dt_cadastro = datetime(
+                    int(data[:4]), int(data[4:6]), int(
+                        data[6:8]),  # YYYY-MM-DD
+                    int(hora[:2]), int(hora[2:4]), int(hora[4:6])  # H-M-S
+                )
+                transacao['dt_cadastro'] = dt_cadastro
+
+                tipo_transacao = TipoTransacao.objects.get(
+                    id=transacao["tipo"])
+                transacao["tipo"] = tipo_transacao
+
+                Transacao.objects.create(**transacao)
 
         return HttpResponse(remessa.transacoes)
